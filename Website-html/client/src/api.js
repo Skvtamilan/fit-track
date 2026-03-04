b424596dfb4146c4e8af65d1d1f4de896563bfb7
Shrijesh-Command <skvtamilan2006@gmail.com>
2026-03-04 16:10:03 +0530

    navaneedhan1

--- Website-html/client/src/api.js
+++ Website-html/client/src/api.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:5000/api';
 
+const API = "https://fit-track-gv7h.onrender.com";
+
+axios.get(`${API}/api/workouts`);
+
 export default axios.create({
   baseURL: API_BASE,
   headers: {
